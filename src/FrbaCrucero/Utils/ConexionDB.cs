79c63997dbcb5404e2b70e6f5c0f2dd8e9e23a8c
nanderson87 <nicolas.anderson87@gmail.com>
2019-05-28 23:47:13 -0700

    Agrega conector a la DB

---
+++ src/FrbaCrucero/Utils/ConexionDB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FrbaCrucero
+{
+	public class ConexionDB
+	{
+		public static ConexionDB instancia = new ConexionDB();
+		private SqlConnection cnn;
+
+        public SqlConnection crearConexion() 
+		{
+            var connectionString = ConfigurationManager.ConnectionStrings["GD1C2019DB"].ConnectionString;
+            return new SqlConnection(connectionString);
+        }
+		
+		public void abrirConexion() 
+		{
+            cnn.Open();
+        }
+		
+		public void cerrarConexion() 
+		{
+            cnn.Close();
+        }
+		
+        public DataTable obtenerData(String query) {
+			try
+			{
+            	crearConexion();
+            	abrirConexion();
+				
+				DataTable tabla = new DataTable();
+            	SqlCommand sqlCmd = new SqlCommand(query, cnn);
+
+            	tabla.Load(sqlCmd.ExecuteReader());
+            	cerrarConexion();
+				
+				return tabla;
+			}
+			catch (Exception)
+			{
+                throw;
+            } 
+        }
+	}
+}
