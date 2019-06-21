09cc217e55b01aea72dfa6b07417cc64ba05990c
gonzaloven <gonzaventura92@gmail.com>
2019-06-21 19:35:31 -0300

    Agrego algo de logica

--- src/FrbaCrucero/AbmRecorrido/AgregarRecorrido.Designer.cs
+++ src/FrbaCrucero/AbmRecorrido/AgregarRecorrido.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.dataGridViewTramosTotales = new System.Windows.Forms.DataGridView();
-            this.Agregar = new System.Windows.Forms.DataGridViewButtonColumn();
             this.buttonGuardar = new System.Windows.Forms.Button();
             this.buttonCancelar = new System.Windows.Forms.Button();
             this.dataGridViewTramosActuales = new System.Windows.Forms.DataGridView();
@@ -38,31 +37,29 @@
             this.label1 = new System.Windows.Forms.Label();
             this.labelCodigo = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Agregar = new System.Windows.Forms.DataGridViewButtonColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTramosTotales)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTramosActuales)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridViewTramosTotales
             // 
+            this.dataGridViewTramosTotales.AllowUserToAddRows = false;
+            this.dataGridViewTramosTotales.AllowUserToDeleteRows = false;
             this.dataGridViewTramosTotales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewTramosTotales.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Agregar});
-            this.dataGridViewTramosTotales.Location = new System.Drawing.Point(480, 89);
+            this.dataGridViewTramosTotales.Location = new System.Drawing.Point(579, 89);
             this.dataGridViewTramosTotales.Name = "dataGridViewTramosTotales";
+            this.dataGridViewTramosTotales.ReadOnly = true;
             this.dataGridViewTramosTotales.RowTemplate.Height = 24;
-            this.dataGridViewTramosTotales.Size = new System.Drawing.Size(379, 411);
+            this.dataGridViewTramosTotales.Size = new System.Drawing.Size(590, 411);
             this.dataGridViewTramosTotales.TabIndex = 0;
-            // 
-            // Agregar
-            // 
-            this.Agregar.HeaderText = "";
-            this.Agregar.Name = "Agregar";
-            this.Agregar.Text = "Agregar";
-            this.Agregar.UseColumnTextForButtonValue = true;
+            this.dataGridViewTramosTotales.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewTramosTotales_CellContentClick);
             // 
             // buttonGuardar
             // 
-            this.buttonGuardar.Location = new System.Drawing.Point(744, 506);
+            this.buttonGuardar.Location = new System.Drawing.Point(1054, 506);
             this.buttonGuardar.Name = "buttonGuardar";
             this.buttonGuardar.Size = new System.Drawing.Size(115, 36);
             this.buttonGuardar.TabIndex = 1;
@@ -71,7 +68,7 @@
             // 
             // buttonCancelar
             // 
-            this.buttonCancelar.Location = new System.Drawing.Point(623, 506);
+            this.buttonCancelar.Location = new System.Drawing.Point(933, 506);
             this.buttonCancelar.Name = "buttonCancelar";
             this.buttonCancelar.Size = new System.Drawing.Size(115, 36);
             this.buttonCancelar.TabIndex = 3;
@@ -80,13 +77,16 @@
             // 
             // dataGridViewTramosActuales
             // 
+            this.dataGridViewTramosActuales.AllowUserToAddRows = false;
+            this.dataGridViewTramosActuales.AllowUserToDeleteRows = false;
             this.dataGridViewTramosActuales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewTramosActuales.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Quitar});
             this.dataGridViewTramosActuales.Location = new System.Drawing.Point(12, 89);
             this.dataGridViewTramosActuales.Name = "dataGridViewTramosActuales";
+            this.dataGridViewTramosActuales.ReadOnly = true;
             this.dataGridViewTramosActuales.RowTemplate.Height = 24;
-            this.dataGridViewTramosActuales.Size = new System.Drawing.Size(339, 411);
+            this.dataGridViewTramosActuales.Size = new System.Drawing.Size(502, 411);
             this.dataGridViewTramosActuales.TabIndex = 4;
             // 
             // Quitar
@@ -108,7 +108,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(477, 59);
+            this.label1.Location = new System.Drawing.Point(576, 59);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(121, 17);
             this.label1.TabIndex = 6;
@@ -130,11 +130,19 @@
             this.textBox1.Size = new System.Drawing.Size(195, 22);
             this.textBox1.TabIndex = 8;
             // 
+            // Agregar
+            // 
+            this.Agregar.HeaderText = "";
+            this.Agregar.Name = "Agregar";
+            this.Agregar.ReadOnly = true;
+            this.Agregar.Text = "Agregar";
+            this.Agregar.UseColumnTextForButtonValue = true;
+            // 
             // AgregarRecorrido
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(871, 554);
+            this.ClientSize = new System.Drawing.Size(1181, 554);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.labelCodigo);
             this.Controls.Add(this.label1);
@@ -155,7 +163,6 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridViewTramosTotales;
-        private System.Windows.Forms.DataGridViewButtonColumn Agregar;
         private System.Windows.Forms.Button buttonGuardar;
         private System.Windows.Forms.Button buttonCancelar;
         private System.Windows.Forms.DataGridView dataGridViewTramosActuales;
@@ -164,5 +171,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label labelCodigo;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewButtonColumn Agregar;
     }
 }
--- src/FrbaCrucero/AbmRecorrido/AgregarRecorrido.cs
+++ src/FrbaCrucero/AbmRecorrido/AgregarRecorrido.cs
@@ -12,10 +12,36 @@ namespace FrbaCrucero.AbmRecorrido
 {
     public partial class AgregarRecorrido : Form
     {
+        DataTable tablaTotal = new DataTable();
         public AgregarRecorrido()
         {
             InitializeComponent();
             Repositorios.RepoTramo.instancia.llenarDatos(dataGridViewTramosTotales);
+            tablaTotal.Columns.Add("id", typeof(Int32));
+            tablaTotal.Columns.Add("puertoDesde", typeof(String));
+            tablaTotal.Columns.Add("puertoHasta", typeof(String));
+            tablaTotal.Columns.Add("precio_base", typeof(Double));
+            dataGridViewTramosActuales.DataSource = tablaTotal;
         }
+
+        private void dataGridViewTramosTotales_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 0)
+            {
+                DataGridViewRow fila = dataGridViewTramosTotales.CurrentRow;
+                this.agregarTramo(fila);
+            }
+        }
+
+        private void agregarTramo(DataGridViewRow fila)
+        {
+           var datarow = ((DataRowView)fila.DataBoundItem).Row;
+           if (!tablaTotal.Rows.Contains(datarow))
+           {
+               tablaTotal.Rows.Add(datarow.ItemArray);
+           }
+        }
+
+
     }
 }
