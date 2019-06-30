4cd5ceabf4b4d38e31c4b5216a4754adb5bad4e8
ignacioreyesfs <ignacioreyesfs@gmail.com>
2019-06-30 20:51:06 -0300

    informacion al cliente id reserva

--- src/FrbaCrucero/CompraReservaPasaje/SeleccionarCompraOReserva.cs
+++ src/FrbaCrucero/CompraReservaPasaje/SeleccionarCompraOReserva.cs
@@ -36,7 +36,7 @@ namespace FrbaCrucero.CompraReservaPasaje
                 pasaje.pasajeros--;
             }
 
-            MessageBox.Show("La reserva del pasaje resulto exitosa.", "Exito",
+            MessageBox.Show("La reserva del pasaje resulto exitosa. El codigo para su pago es " + idReserva, "Exito",
                                 MessageBoxButtons.OK, MessageBoxIcon.None);
             this.Close();
         }
