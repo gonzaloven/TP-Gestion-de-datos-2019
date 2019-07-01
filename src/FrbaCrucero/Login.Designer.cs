d23feacbfc3a406d3a47ad6a2cf944784cd71072
gonzaloven <gonzaventura92@gmail.com>
2019-07-01 17:44:25 -0300

    Fixes

--- src/FrbaCrucero/Login.Designer.cs
+++ src/FrbaCrucero/Login.Designer.cs
@@ -47,29 +47,32 @@
             // 
             this.tabControlLogin.Controls.Add(this.tabPage1);
             this.tabControlLogin.Controls.Add(this.tabPage2);
-            this.tabControlLogin.Location = new System.Drawing.Point(12, 98);
+            this.tabControlLogin.Location = new System.Drawing.Point(16, 121);
+            this.tabControlLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabControlLogin.Multiline = true;
             this.tabControlLogin.Name = "tabControlLogin";
             this.tabControlLogin.SelectedIndex = 0;
-            this.tabControlLogin.Size = new System.Drawing.Size(267, 274);
+            this.tabControlLogin.Size = new System.Drawing.Size(356, 337);
             this.tabControlLogin.TabIndex = 0;
             // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.buttonIngresarCliente);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(259, 248);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(348, 308);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Cliente";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // buttonIngresarCliente
             // 
-            this.buttonIngresarCliente.Location = new System.Drawing.Point(85, 111);
+            this.buttonIngresarCliente.Location = new System.Drawing.Point(113, 137);
+            this.buttonIngresarCliente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonIngresarCliente.Name = "buttonIngresarCliente";
-            this.buttonIngresarCliente.Size = new System.Drawing.Size(75, 23);
+            this.buttonIngresarCliente.Size = new System.Drawing.Size(100, 28);
             this.buttonIngresarCliente.TabIndex = 0;
             this.buttonIngresarCliente.Text = "Ingresa";
             this.buttonIngresarCliente.UseVisualStyleBackColor = true;
@@ -82,10 +85,11 @@
             this.tabPage2.Controls.Add(this.textBoxAdmUsario);
             this.tabPage2.Controls.Add(this.labelUsuario);
             this.tabPage2.Controls.Add(this.buttonIngresarAdm);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(259, 248);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Size = new System.Drawing.Size(348, 308);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Administrador";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -93,44 +97,49 @@
             // textBoxAdmPassword
             // 
             this.textBoxAdmPassword.AcceptsTab = true;
-            this.textBoxAdmPassword.Location = new System.Drawing.Point(9, 85);
+            this.textBoxAdmPassword.Location = new System.Drawing.Point(12, 105);
+            this.textBoxAdmPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxAdmPassword.Name = "textBoxAdmPassword";
-            this.textBoxAdmPassword.Size = new System.Drawing.Size(204, 20);
+            this.textBoxAdmPassword.Size = new System.Drawing.Size(271, 22);
             this.textBoxAdmPassword.TabIndex = 4;
             this.textBoxAdmPassword.UseSystemPasswordChar = true;
             // 
             // labelPassword
             // 
             this.labelPassword.AutoSize = true;
-            this.labelPassword.Location = new System.Drawing.Point(6, 69);
+            this.labelPassword.Location = new System.Drawing.Point(8, 85);
+            this.labelPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelPassword.Name = "labelPassword";
-            this.labelPassword.Size = new System.Drawing.Size(53, 13);
+            this.labelPassword.Size = new System.Drawing.Size(69, 17);
             this.labelPassword.TabIndex = 3;
             this.labelPassword.Text = "Password";
             // 
             // textBoxAdmUsario
             // 
             this.textBoxAdmUsario.AcceptsTab = true;
-            this.textBoxAdmUsario.Location = new System.Drawing.Point(9, 26);
+            this.textBoxAdmUsario.Location = new System.Drawing.Point(12, 32);
+            this.textBoxAdmUsario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxAdmUsario.Name = "textBoxAdmUsario";
-            this.textBoxAdmUsario.Size = new System.Drawing.Size(204, 20);
+            this.textBoxAdmUsario.Size = new System.Drawing.Size(271, 22);
             this.textBoxAdmUsario.TabIndex = 2;
             this.textBoxAdmUsario.Tag = "";
             // 
             // labelUsuario
             // 
             this.labelUsuario.AutoSize = true;
-            this.labelUsuario.Location = new System.Drawing.Point(6, 10);
+            this.labelUsuario.Location = new System.Drawing.Point(8, 12);
+            this.labelUsuario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelUsuario.Name = "labelUsuario";
-            this.labelUsuario.Size = new System.Drawing.Size(43, 13);
+            this.labelUsuario.Size = new System.Drawing.Size(57, 17);
             this.labelUsuario.TabIndex = 1;
             this.labelUsuario.Text = "Usuario";
             // 
             // buttonIngresarAdm
             // 
-            this.buttonIngresarAdm.Location = new System.Drawing.Point(80, 207);
+            this.buttonIngresarAdm.Location = new System.Drawing.Point(107, 255);
+            this.buttonIngresarAdm.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonIngresarAdm.Name = "buttonIngresarAdm";
-            this.buttonIngresarAdm.Size = new System.Drawing.Size(75, 23);
+            this.buttonIngresarAdm.Size = new System.Drawing.Size(100, 28);
             this.buttonIngresarAdm.TabIndex = 0;
             this.buttonIngresarAdm.Text = "Ingresar";
             this.buttonIngresarAdm.UseVisualStyleBackColor = true;
@@ -140,20 +149,22 @@
             // 
             this.labelTituloIngreso.AutoSize = true;
             this.labelTituloIngreso.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTituloIngreso.Location = new System.Drawing.Point(53, 37);
+            this.labelTituloIngreso.Location = new System.Drawing.Point(71, 46);
+            this.labelTituloIngreso.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelTituloIngreso.Name = "labelTituloIngreso";
-            this.labelTituloIngreso.Size = new System.Drawing.Size(178, 24);
+            this.labelTituloIngreso.Size = new System.Drawing.Size(226, 29);
             this.labelTituloIngreso.TabIndex = 1;
             this.labelTituloIngreso.Text = "Ingresar en Crucero";
             this.labelTituloIngreso.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(291, 384);
+            this.ClientSize = new System.Drawing.Size(388, 473);
             this.Controls.Add(this.labelTituloIngreso);
             this.Controls.Add(this.tabControlLogin);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Login";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
